a1b4096bb1177cfc378a4f26040cc723a2b855f2
jjacuna <47874680+jjacuna@users.noreply.github.com>
2019-03-12 15:19:10 -0700

    Added filter method on robots array

---
+++ src/CardList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { robots } from "./Robots.js";
+import Card from "./Card.js";
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((user, i) => {
+        return (
+          <Card
+            key={i}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardList;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"; //the view window
+import ReactDOM from "react-dom"; //
+import "./index.css";
+// import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import App from "./App.js";
+import CardList from "./CardList";
+import { robots } from "./Robots.js";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const reactElement = <h1>Hello World</h1>;
+// ReactDOM.render(reactElement, document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
